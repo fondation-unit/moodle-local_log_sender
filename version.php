f789a2f049f82b65bc75a3381c0219e277efeab1
Hipjea <pierre.duverneix@gmail.com>
2026-02-11 15:04:31 +0100

    add plugin maturity

--- version.php
+++ version.php
@@ -25,5 +25,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_log_sender'; // Full name of the plugin (used for diagnostics).
-$plugin->version  = 2026021101;          // The current module version (Date: YYYYMMDDXX).
+$plugin->version  = 2026021102;          // The current module version (Date: YYYYMMDDXX).
 $plugin->requires = 2023100903;          // Requires this Moodle version.
+$plugin->maturity = MATURITY_BETA;
